320154e06271fe74dbe6fa4076262c53fe5fe7f9
Jay <123050539+Kingjaybail@users.noreply.github.com>
2023-10-30 16:23:16 -0500

    The whole thing
    First wit gab at commos

---
+++ I Challenge Thee!/Assets/Scripts/Shooting.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class Shooting: MonoBehaviour
+{
+    public GameObject targetObject;
+    public GameObject bulletPrefab; // Assign the bullet prefab in the Inspector.
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            ShootScript();
+        }
+    }
+    private void ShootScript()
+    {
+        if (targetObject != null)
+        {
+            Vector3 targetPosition = targetObject.transform.position;
+            Quaternion targetRotation = targetObject.transform.rotation;
+
+            // Instantiate the bullet at the target position and rotation.
+            GameObject bullet = Instantiate(bulletPrefab, targetPosition, targetRotation);
+
+            // Apply a force to the bullet.
+            Rigidbody bulletRigidbody = bullet.GetComponent<Rigidbody>();
+            if (bulletRigidbody != null)
+            {
+                bulletRigidbody.AddForce(bullet.transform.forward * 120, ForceMode.VelocityChange);
+            }
+
+            Destroy(bullet, 3f);
+        }
+    }
+}
